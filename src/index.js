fe48f131166fd35c388dcac3899263fa1af2645b
Rodrigo Juliani <srodriki@gmail.com>
2016-08-31 17:53:07 -0300

    #1 importing Vue in main module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 //TODO: Setup vue-bootstrap and create Vue's install function
 
+
 import $ from 'jquery';
 import bootstrap from 'bootstrap';
+import Vue from "vue";
 
 module.exports = {
     name: "vue-bootstrap";
