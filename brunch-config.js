a310d7f363494719cf57767d7f0a527c0899bd1f
Rodrigo Juliani <srodriki@gmail.com>
2016-09-01 12:39:13 -0300

    #1 #2 It builds now!

--- brunch-config.js
+++ brunch-config.js
@@ -1,15 +1,26 @@
 module.exports = {
+    paths: {
+        public: 'dist',
+        watched: ['src', 'test', 'vendor']
+    },
     files: {
         javascripts: {
-            joinTo: 'vue-bootstrap.js'
+            joinTo: {
+                'vue-bootstrap.js': /^src/
+            }
         },
         templates: {
-            joinTo: 'vue-bootstrap.js'
+            joinTo: {
+                'vue-bootstrap.js': /^src/
+            }
         }
     },
     plugins: {
         babel: {
             presets: ['es2015']
+        },
+        uglify: {
+            mangle: false
         }
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-//TODO: Setup vue-bootstrap and create Vue's install function
+//The following imports have been commented out for now, as I believe they are not necessary.
+//TODO: Check if these imports are needed in POC. If not => remove them completely
+//import $ from 'jquery';
+//import bootstrap from 'bootstrap';
 
-
-import $ from 'jquery';
-import bootstrap from 'bootstrap';
 import Vue from "vue";
 
-module.exports = {
-    name: "vue-bootstrap";
+// This is a list of all our Vue components
+export const components = {
+
+};
+
+//This is a list of all our Vue directives
+export const directives = {
+
+};
+
+export default {
+    /**
+     * This is vue's install method. It is called by Vue's engine when you do Vue.use
+     * so this plugin is included
+     * @param  {Object]} Vue Vue instance
+     * @return {None}    This method doesn't return a value
+     */
+    install(Vue) {
+        //This registers the components and directives in this library with Vue
+        //so they can be used in markup directly and without having to do not
+        //need to be added later upon definition of another components.
+        Object.keys(components).forEach((name) => {
+            Vue.component(name, components[name]);
+        });
+        Object.keys(directives).forEach((name) => {
+            Vue.directive(name, directives[name]);
+        });
+    }
+}
+
+// Export all components and directives sepparetely.
+export {
+
 }
